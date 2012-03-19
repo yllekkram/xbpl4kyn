eccea9c14d599fdb264b860c566a9560259ab00c
Sarangan Bala <saranganxyz.sb@gmail.com>
2012-03-18 20:38:31 -0400

    updated main status thread.

--- elevator_controller/include/ElevatorSimulator.hpp
+++ elevator_controller/include/ElevatorSimulator.hpp
@@ -1,10 +1,10 @@
-#define MAX_SPEED		4			//maximum speed of elevator
-#define ACCELERATION	2		//acceleration of elevator
-#define DECELATRAION	-2		//Deceleration of elevator
+#define MAX_SPEED	4			//maximum speed of elevator
+#define ACCELERATION	0.64			//acceleration of elevator
+#define DECELATRAION	-0.64			//Deceleration of elevator
 #define FLOOR_HEIGHT	10			//height of floor
 #define DOOR_OPEN_TIME	2			//time take to open door
 #define DOOR_CLOSE_TIME	2			//time until the door is open for
-#define IN_OUT_TIME		2			//time take to close door
+#define IN_OUT_TIME	2			//time take to close door
 
 class ElevatorSimulator {
 	public:
@@ -15,6 +15,7 @@ class ElevatorSimulator {
 		void calculateValues();
 		void setFinalDestination(int floorNum);
 		float getCurrentSpeed();
+		float geCurrentPosition();
 		int getCurrentFloor();
 		bool getIsTaskActive();
 		bool getIsDirectionUp();
@@ -27,13 +28,13 @@ class ElevatorSimulator {
 	private:
 		struct timeb *tp;			//start time structure
 		struct timeb *tpe;			//current time structure
-		float currentPosition;		//is the current height in meter
+		float currentPosition;			//is the current height in meter
 		float currentSpeed;			//is the current speed of the elevator in m/s
-		float tempCurrentPosition;	//temp calue of current location at the start of a task (used for down ward calculations)
+		float tempCurrentPosition;		//temp calue of current location at the start of a task (used for down ward calculations)
 		int currentFloor;			//is the current height in meter
-		int finalDestination;		//final destination is the final floor number
+		int finalDestination;			//final destination is the final floor number
 		float midLocation;			//final destination is the final floor number
-		bool task;					//is there a task available
+		bool task;				//is there a task available
 		bool directionUP;			//is elevator traveling up
 		bool DoorOpening;			//is elevator door opening
 		bool DoorOpen;				//is elevator door open
@@ -47,9 +48,9 @@ class ElevatorSimulator {
 		float distDec;				//the deceleration distance from maximum speed to zero
 		float distMaxSpeed;			//maximum distance spent on maximum speed
 		float travelTime;			//total time spent traveling
-		float doorOpenedTime;		//time until door open for current task
-		float doorOpenedAndReadyTime;	//time until door open until close for current task
-		float doorClosedTime;	//time until door close for current task
-		float midTime;			//mid time before acceleration switch to deceleration movement
-		float midSpeed;			//mid speed before acceleration switch to deceleration movement
+		float doorOpenedTime;			//time until door open for current task
+		float doorOpenedAndReadyTime;		//time until door open until close for current task
+		float doorClosedTime;			//time until door close for current task
+		float midTime;				//mid time before acceleration switch to deceleration movement
+		float midSpeed;				//mid speed before acceleration switch to deceleration movement
 };	
