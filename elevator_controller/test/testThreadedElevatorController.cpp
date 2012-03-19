3b8918917d3817d2d2af948cea3934977764aafe
Mark Kelly <markkelly0808@gmail.com>
2012-03-18 23:02:00 -0400

    works with objects declared globally and with mlockall

--- elevator_controller/test/testThreadedElevatorController.cpp
+++ elevator_controller/test/testThreadedElevatorController.cpp
@@ -1,17 +1,11 @@
-#include <native/task.h>
-#include <signal.h>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
 #include <sys/mman.h>
 
 #include "ElevatorController.hpp"
 #include "Exception.hpp"
 #include "UDPView.hpp"
-
-void run(void*);
-void jog(void*);
-void catch_signal(int);
-
-#define NUM_TASKS 1
-RT_TASK task[NUM_TASKS];
 
 ElevatorController ec;
 UDPView uv;
@@ -19,38 +13,22 @@ UDPView uv;
 int main(int argc, char* argv[]) {
 	mlockall(MCL_CURRENT|MCL_FUTURE);
 
-	if (argc != 2) {
-		std::cerr << "USAGE: testThreadedElevatorController <gd_ip>" << std::endl;
+	if (argc != 5) {
+		std::cerr << "USAGE: main <gd_ip> <gd_port> <gui_ip> <gui_port>" << std::endl;
 		exit(1);
 	}
 
-	// for (int i = 0; i < NUM_TASKS; i++) {
-	// 	rt_task_create(&task[i], NULL, 0, 99, T_JOINABLE);
-	// }
+	uv.init(argv[3], argv[4]);
 
-	ec.connectToGD(argv[1], 5000);
+	ec.connectToGD(argv[1], atoi(argv[2]));
+  try {
+    ec.addView(&uv);
+  }
+  catch (Exception e) {}
 	
-	uv.init("192.168.251.1", "5000");
-	try {
-		ec.addView(&uv);
-	}
-	catch (Exception e) {}
-
 	uv.run();
 
-	// rt_task_start(&task[0], &run, &ec);
+	std::cout << std::endl;
 
-	return 0;
-}
-
-void run(void* cookie) {
-	while (true) {
-		printf("%d ", ((ElevatorController*)cookie)->getID());
-	}
-}
-
-void catch_signal(int sig) {
-	for (int i = 0; i < NUM_TASKS; i++) {
-		rt_task_delete(&task[i]);
-	}
+	exit(0);
 }
