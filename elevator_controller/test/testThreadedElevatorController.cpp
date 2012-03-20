0723430bfc931aaaac8c060a11a3bf5c1d88e02d
Mark Kelly <markkelly0808@gmail.com>
2012-03-20 01:28:51 -0400

    UDP mostly works in thread

--- elevator_controller/test/testThreadedElevatorController.cpp
+++ elevator_controller/test/testThreadedElevatorController.cpp
@@ -1,22 +1,38 @@
 #include <cstring>
 #include <cstdlib>
 #include <iostream>
+#include <native/task.h>
+#include <signal.h>
 #include <sys/mman.h>
 
 #include "ElevatorController.hpp"
 #include "Exception.hpp"
 #include "UDPView.hpp"
 
+/* Function Headers */
+void catch_signal(int);
+void runUDPThread(void*);
+/* End Function Headers */
+
+/* Data Declaration */
+RT_TASK udpThread;
+
 ElevatorController ec;
 UDPView uv;
+/* End Data Declaration */
 
 int main(int argc, char* argv[]) {
 	mlockall(MCL_CURRENT|MCL_FUTURE);
+
+	signal(SIGINT, catch_signal);
+	signal(SIGTERM, catch_signal);
 
 	if (argc != 5) {
 		std::cerr << "USAGE: main <gd_ip> <gd_port> <gui_ip> <gui_port>" << std::endl;
 		exit(1);
 	}
+
+	rt_task_create(&udpThread, NULL, 0, 99, T_JOINABLE);
 
 	uv.init(argv[3], argv[4]);
 
@@ -25,10 +41,19 @@ int main(int argc, char* argv[]) {
     ec.addView(&uv);
   }
   catch (Exception e) {}
+
+	rt_task_start(&udpThread, &runUDPThread, NULL);
+
+	rt_task_join(&udpThread);
 	
-	uv.run();
-
-	std::cout << std::endl;
-
 	exit(0);
+}
+
+void catch_signal(int sig) {
+	rt_task_delete(&udpThread);
+}
+
+void runUDPThread(void* cookie) {
+	printf("UDP Thread\n");
+	uv.run();
 }
